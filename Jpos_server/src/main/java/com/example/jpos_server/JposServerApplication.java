85249fc57a69502ca7f2fa9b83b9234983dd4e0f
전찬혁 <jchn0111@naver.com>
2023-11-13 19:10:08 +0900

    웹서버 프로젝트 생성 및 gitignore 설정

---
+++ Jpos_server/src/main/java/com/example/jpos_server/JposServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpos_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JposServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JposServerApplication.class, args);
+	}
+
+}
