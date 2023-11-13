db242dfc4790482e4cc53b3cb51296223adf20e2
전찬혁 <jchn0111@naver.com>
2023-11-13 21:53:04 +0900

    #10 - data rest 설정

--- Jpos_server/src/main/java/com/example/jpos_server/domain/Menu.java
+++ Jpos_server/src/main/java/com/example/jpos_server/domain/Menu.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.locationtech.jts.geom.Point;
 
 import java.util.Objects;
 
--- Jpos_server/src/main/java/com/example/jpos_server/repository/MenuRepository.java
+++ Jpos_server/src/main/java/com/example/jpos_server/repository/MenuRepository.java
@@ -2,6 +2,8 @@ package com.example.jpos_server.repository;
 
 import com.example.jpos_server.domain.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface MenuRepository extends JpaRepository<Menu, Long> {
 }
