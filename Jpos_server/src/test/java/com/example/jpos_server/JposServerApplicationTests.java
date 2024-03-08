140ccc090128e69e5aefd21b84df8ba5ee573003
전찬혁 <jchn0111@naver.com>
2024-03-09 05:44:19 +0900

    헤로쿠 설정 추가

--- Jpos_server/src/test/java/com/example/jpos_server/JposServerApplicationTests.java
+++ Jpos_server/src/test/java/com/example/jpos_server/JposServerApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.jpos_server;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JposServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
